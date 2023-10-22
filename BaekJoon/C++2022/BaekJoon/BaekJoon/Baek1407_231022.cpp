4484a53d65f0c6fbb59f2ca6999f272a2058e138
SooLight <soso1461@naver.com>
2023-10-23 00:04:20 +0900

    1407 complete
    long long 잘 활용하자..

--- BaekJoon/C++2022/BaekJoon/BaekJoon/Baek1407_231022.cpp
+++ BaekJoon/C++2022/BaekJoon/BaekJoon/Baek1407_231022.cpp
@@ -7,20 +7,20 @@ int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int A, B;
+	long long A, B;
 	cin >> A >> B;
 
 	A -= 1;
 
-	int	tmp_A = A;
-	for (int i = 1; i < 100; i++) {
-		tmp_A += (A / pow(2, i)) * (pow(2, i) - pow(2, i - 1));
+	long long	tmp_A = A;
+	for (int i = 1; i < 50; i++) {
+		tmp_A += (A / (long long)pow(2, i)) * ((long long)pow(2, i) - (long long)pow(2, i - 1));
 	}
 
-	int tmp_B = B;
-	for (int i = 1; i < 100; i++) {
-		tmp_B += (B / pow(2, i)) * (pow(2, i) - pow(2, i - 1));
+	long long tmp_B = B;
+	for (int i = 1; i < 50; i++) {
+		tmp_B += (B / (long long)pow(2, i)) * ((long long)pow(2, i) - (long long)pow(2, i - 1));
 	}
 
-	cout << (tmp_B - tmp_A);
+	cout << tmp_B - tmp_A << '\n';
 }
