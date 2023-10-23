6dbe7538b9e808d4eac2b6cced20f3a535b71ae2
SooLight <soso1461@naver.com>
2023-10-23 19:43:34 +0900

    11660 푸는중

---
+++ BaekJoon/C++2022/BaekJoon/BaekJoon/Baek11660_231023.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	vector<vector<int>> graph(4, vector<int>(4));
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			cin >> graph[i][j];
+		}
+	}
+	
+}
