dac7ceb885e3aa25300cd6d4009c1468e74730e9
SooLight <72859594+soso1461@users.noreply.github.com>
2024-03-06 21:44:14 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(long n) {
+        long answer = (long)Math.sqrt(n);
+        
+        if (answer * answer == n) {
+            return (answer + 1) * (answer + 1);
+        } else {
+            answer = -1;
+        }
+        
+        return answer;
+    }
+}
