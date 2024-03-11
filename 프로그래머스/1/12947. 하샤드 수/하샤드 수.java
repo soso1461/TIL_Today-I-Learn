7522ed41a9b4f42145a477a3361b2361cf8321fe
SooLight <72859594+soso1461@users.noreply.github.com>
2024-03-11 23:23:11 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int sum = 0;
+        int tmpX = x;
+        
+        while (x > 0) {
+            sum += x % 10;
+            x /= 10;
+        }        
+        
+        if (tmpX % sum != 0) {
+            answer = false;
+        }
+        
+        return answer;
+    }
+}
