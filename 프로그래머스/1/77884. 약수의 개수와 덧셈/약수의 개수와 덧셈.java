f5cfa992a10b7b8259168a1d6caaf3927f1dfc12
SooLight <soso1461@naver.com>
2024-03-18 02:54:10 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.28 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        
+        while (left <= right) {
+            int count = 0;
+            for (int i = 1; i <= left; i++) {
+                if (left % i == 0) {
+                    count++;
+                }
+            }
+            if (count % 2 == 0) {
+                answer += left;
+            } else answer -= left;
+            left++;
+        }
+        
+        return answer;
+    }
+}
