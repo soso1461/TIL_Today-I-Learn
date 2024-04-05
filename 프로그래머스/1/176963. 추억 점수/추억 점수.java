42bee7ba4fabdef1be229e89af66e48dde4516b5
SooLight <soso1461@naver.com>
2024-04-06 00:01:10 +0900

    [level 1] Title: 추억 점수, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,15 +1,6 @@
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] answer = new int[photo.length];
-        
-        for(int i = 0; i < photo.length; i++) {
-        	for(int j = 0; j < photo[i].length; j++) {
-        		for(int k = 0; k < name.length; k++) {
-        			if (photo[i][j].equals(name[k]))
-                        answer[i] += yearning[k];
-        		}
-        	}
-        }
+        int[] answer = {};
         return answer;
     }
 }
