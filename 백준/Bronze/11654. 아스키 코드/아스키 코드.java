c902d04a20842652e630f880255cce7a610d8124
SooLight <soso1461@naver.com>
2024-05-30 17:47:58 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17584 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.next().charAt(0);
+        System.out.print(A);
+    }
+}
